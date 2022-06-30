907dd3473301d40b2fe16c4aa4225c53b3224719
Boffeer <41575886+Boffeer@users.noreply.github.com>
2022-06-30 10:25:02 +0300

    make pagination

--- archive.php
+++ archive.php
@@ -24,9 +24,10 @@ get_header();
 	</div>
 </main>
 
-<section class="news-page">
+<section class="<?php echo get_queried_object()->name . '-page'; ?>">
 	<div class="container">
-		<h1><?php echo get_queried_object()->label ?></h1>
+		<h1><?php echo get_queried_object()->label
+				?></h1>
 		<div class="news-page__box">
 			<div class="section_news__container">
 				<?php if (have_posts()) : ?>
@@ -44,8 +45,6 @@ get_header();
 
 					endwhile;
 
-					the_posts_navigation();
-
 				else :
 
 					get_template_part('template-parts/content', 'none');
@@ -53,25 +52,14 @@ get_header();
 				endif; ?>
 			</div>
 		</div>
-		<div class="pagination fl-align">
-			<button class="pagination-prev">
-				<svg width="12" height="22" viewBox="0 0 12 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-					<path d="M11 21L1 11L11 1" stroke="#222222" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-				</svg>
-			</button>
-			<ul class="pagination__list fl-align">
-				<li class="pagination__list-element _active">1</li>
-				<li class="pagination__list-element">2</li>
-				<li class="pagination__list-element">3</li>
-				<li class="pagination__list-element">...</li>
-				<li class="pagination__list-element">10</li>
-			</ul>
-			<button class="pagination-next">
-				<svg width="12" height="22" viewBox="0 0 12 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-					<path d="M1 21L11 11L1 1" stroke="#222222" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-				</svg>
-			</button>
-		</div>
+		<?php if (zenit_get_pagination()) : ?>
+			<div class="pagination fl-align">
+				<?php echo zenit_get_pagination();  ?>
+			</div>
+		<?php endif; ?>
+		<?php if (get_queried_object()->name != 'news') {
+			echo zenit_catalog_button();
+		} ?>
 	</div>
 </section>
 
--- inc/template-functions.php
+++ inc/template-functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Functions which enhance the theme by hooking into WordPress
  *
@@ -11,27 +12,90 @@
  * @param array $classes Classes for the body element.
  * @return array
  */
-function zenit_body_classes( $classes ) {
+function zenit_body_classes($classes)
+{
 	// Adds a class of hfeed to non-singular pages.
-	if ( ! is_singular() ) {
+	if (!is_singular()) {
 		$classes[] = 'hfeed';
 	}
 
 	// Adds a class of no-sidebar when there is no sidebar present.
-	if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+	if (!is_active_sidebar('sidebar-1')) {
 		$classes[] = 'no-sidebar';
 	}
 
 	return $classes;
 }
-add_filter( 'body_class', 'zenit_body_classes' );
+add_filter('body_class', 'zenit_body_classes');
 
 /**
  * Add a pingback url auto-discovery header for single posts, pages, or attachments.
  */
-function zenit_pingback_header() {
-	if ( is_singular() && pings_open() ) {
-		printf( '<link rel="pingback" href="%s">', esc_url( get_bloginfo( 'pingback_url' ) ) );
+function zenit_pingback_header()
+{
+	if (is_singular() && pings_open()) {
+		printf('<link rel="pingback" href="%s">', esc_url(get_bloginfo('pingback_url')));
 	}
 }
-add_action( 'wp_head', 'zenit_pingback_header' );
+add_action('wp_head', 'zenit_pingback_header');
+
+function zenit_custom_pagination($nav)
+{
+	$search  = [
+		'<nav class="navigation pagination" role="navigation">',
+		'<div class="nav-links">',
+		'</div>',
+		'<a class="page-numbers"',
+		'<a class="prev page-numbers"',
+		'<a class="next page-numbers"',
+		'<span aria-current="page" class="page-numbers current"',
+		'<span class="page-numbers dots">',
+	];
+	$replace = [
+		'<nav>',
+		'<div class="icons-list__pagination flex pagination">',
+		'</div>',
+		'<a class="pagination__list-element"',
+		'<a class="pagination-prev"',
+		'<a class="pagination-next"',
+		'<span aria-current="page" class="pagination__list-element _active"',
+		'<span class="pagination__list-element">',
+	];
+
+	/*
+			<button class="pagination-prev">
+			</button>
+			<ul class="pagination__list fl-align">
+				<li class="pagination__list-element _active">1</li>
+				<li class="">2</li>
+				<li class="pagination__list-element">3</li>
+				<li class="pagination__list-element">...</li>
+				<li class="pagination__list-element">10</li>
+			</ul>
+			<button class="pagination-next">
+				<svg width="12" height="22" viewBox="0 0 12 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path d="M1 21L11 11L1 1" stroke="#222222" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+				</svg>
+			</button>
+			*/
+	$nav  = str_replace($search, $replace, $nav);
+	return $nav;
+}
+
+function zenit_get_pagination()
+{
+	global $paged;
+	// global $query;
+	$paged = max($paged, 1);
+	$per_page = 10;
+	$total = ceil($query->found_posts / $paged);
+	$args = array(
+		// 'total'        => 1,
+		// 'current'      => $paged,
+		'prev_text'    => '<svg width="12" height="22" viewBox="0 0 12 22" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M11 21L1 11L11 1" stroke="#222222" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" /> </svg>',
+		'next_text'    => '<svg width="12" height="22" viewBox="0 0 12 22" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M1 21L11 11L1 1" stroke="#222222" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" /> </svg>',
+
+
+	);
+	return zenit_custom_pagination(paginate_links($args));
+}
